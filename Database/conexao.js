45e9c2251d3d9c1a31a5776fa6c360c490d81987
CALEAO\caleao <cacleaof@gmail.com>
2025-06-13 11:36:18 -0300

    pool

--- Database/conexao.js
+++ Database/conexao.js
@@ -1,11 +1,14 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-const conexao = mysql.createConnection({
+const conexao = mysql.createPool({
     host: 'mysql-caleao.alwaysdata.net',
     port: 3306,
     user: 'caleao',
     password: 'caleao428027',               
-    database: 'caleao_controle'
+    database: 'caleao_controle',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
 });
 
 module.exports = conexao;
